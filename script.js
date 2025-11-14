1da58d2ce5e4f1d0c991e30328974c90e7dc45a2
Manoj-CS <153212418+Manoj-Jakkampudi@users.noreply.github.com>
2025-11-14 14:25:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+ const apiKey = "360e26d813bf7d3cb9524320d14b29a2";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+  
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+async function getWeatherByLocation(city){
+
+         const resp = await fetch(url(city));
+         const respData = await resp.json();
+
+         if (resp.ok) {
+             addWeatherToPage(respData);
+         } else {
+             main.innerHTML = `<p>Error: ${respData.message}</p>`;
+         }
+
+     }
+
+      function addWeatherToPage(data){
+          const temp = Ktoc(data.main.temp);
+
+          const weather = document.createElement('div')
+          weather.classList.add('weather');
+
+          weather.innerHTML = `
+          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+          <small>${data.weather[0].main}</small>
+          
+          `;
+
+
+        //   cleanup 
+          main.innerHTML= "";
+           main.appendChild(weather);
+      };
+
+
+     function Ktoc(K){
+         return Math.floor(K - 273.15);
+     }
+
+
+
+     form.addEventListener('submit',(e) =>{
+        e.preventDefault();
+
+        const city = search.value;
+
+        if(city){
+            getWeatherByLocation(city)
+        }
+
+     });
